4f96e1a837901f56aa19f5eb627e50b06c9eb2e4
Iuliia Ambrokh <iuliia.ambrokh@spsmail.cuny.edu>
2016-10-13 01:15:19 +0000

    Task_01

---
+++ task_01.py
@@ -0,0 +1,14 @@
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Task_01"""
+
+ELEMENTS = (None, 'Hydrogen', 'Helium', 'Lithium', 'Beryllium', 'Boron',
+            'Carbon')
+
+OPERATIONS_ORDER = ['Parentheses',
+                    'Exponents',
+                    'Multiplication',
+                    'Division',
+                    'Addition',
+                    'Subtraction'
+                    ]
